61063bf243357a8b6a795ccd7a6bd5e65b8c46d6
abdelali <abbouchiabdelali02@gmail.com>
2023-10-19 22:05:25 +0200

    ttt

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import copy from "copy-to-clipboard";
 const Index = () => {
   const [videoURL, setVideoURL] = useState("");
   const [thumbnailOptions, setThumbnailOptions] = useState([]);
+  const [currentLanguage, setCurrentLanguage] = useState("en");
 
   const getYouTubeThumbnail = (url) => {
     let regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
@@ -33,17 +34,29 @@ const Index = () => {
     }
   };
 
+  const toggleLanguage = () => {
+    setCurrentLanguage(currentLanguage === "en" ? "fr" : "en");
+  };
+
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 ">
+       
+
       <header className="mb-8" style={{ width: '47%' }}>
         <h1 className="text-3xl font-bold mb-2">
-          Free YouTube Thumbnail Downloader - High Quality, 4K, HD Thumbnails
+          {currentLanguage === "en"
+            ? "Free YouTube Thumbnail Downloader - High Quality, 4K, HD Thumbnails"
+            : "Téléchargeur de Miniatures YouTube Gratuit - Haute Qualité, 4K, Miniatures HD"}
         </h1>
         <p className="text-gray-700">
-          Elevate Your Content with High-Resolution YouTube Video Thumbnails!
+          {currentLanguage === "en"
+            ? "Elevate Your Content with High-Resolution YouTube Video Thumbnails!"
+            : "Valorisez Votre Contenu avec des Miniatures de Vidéo YouTube en Haute Résolution!"}
         </p>
         <p className="text-gray-700">
-          YouTube Thumbnail Downloader is a free online tool that allows you to easily download YouTube video thumbnails in different sizes and qualities. All you need to do is copy a YouTube Video link, paste it in the given input box, and then press "Download YouTube Thumbnail" Your Thumbnail is ready to be downloaded in HD quality. You can also view the sample of the your YouTube video thumbnail. It supports all YouTube videos including 8K, 4K, 1080p, HD, HQ, etc.
+          {currentLanguage === "en"
+            ? "YouTube Thumbnail Downloader is a free online tool that allows you to easily download YouTube video thumbnails in different sizes and qualities. All you need to do is copy a YouTube Video link, paste it in the given input box, and then press 'Download YouTube Thumbnail' Your Thumbnail is ready to be downloaded in HD quality. You can also view the sample of the your YouTube video thumbnail. It supports all YouTube videos including 8K, 4K, 1080p, HD, HQ, etc."
+            : "Le téléchargeur de miniatures YouTube est un outil gratuit en ligne qui vous permet de télécharger facilement des miniatures de vidéos YouTube de différentes tailles et qualités. Il vous suffit de copier un lien vidéo YouTube, de le coller dans la zone de saisie prévue à cet effet, puis de cliquer sur 'Télécharger la Miniature YouTube'. Votre miniature est prête à être téléchargée en haute qualité. Vous pouvez également visualiser l'échantillon de la miniature de votre vidéo YouTube. Il prend en charge toutes les vidéos YouTube, y compris celles en 8K, 4K, 1080p, HD, HQ, etc."}
         </p>
       </header>
 
@@ -52,7 +65,7 @@ const Index = () => {
           <input
             type="text"
             className="w-full md:w-1/2 px-4 py-2 border rounded"
-            placeholder="Enter YouTube URL"
+            placeholder={currentLanguage === "en" ? "Enter YouTube URL" : "Entrez l'URL de la Vidéo YouTube"}
             value={videoURL}
             onChange={(e) => setVideoURL(e.target.value)}
           />
@@ -61,14 +74,19 @@ const Index = () => {
             style={{ fontSize: '16px', backgroundColor: '#FF5733', borderRadius: '8px' }}
             onClick={() => getYouTubeThumbnail(videoURL)}
           >
-            Download YouTube Thumbnail
+            {currentLanguage === "en" ? "Download YouTube Thumbnail" : "Télécharger la Miniature YouTube"}
           </button>
+          <button onClick={toggleLanguage} className="btn-blue mt-2">
+        {currentLanguage === "en" ? "Toggle Language" : "Changer de Langue"}
+      </button>
         </div>
       </div>
 
       {thumbnailOptions.length > 0 && (
         <div className="mt-8">
-          <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
+          <h2 className="text-xl font-semibold mb-4">
+            {currentLanguage === "en" ? "Thumbnail Options" : "Options de Miniatures"}
+          </h2>
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             {thumbnailOptions.map((option, index) => (
               <div key={index} className="thumbnail-option">
@@ -77,74 +95,31 @@ const Index = () => {
                   className="btn-blue mt-2"
                   onClick={() => copy(option.url)}
                 >
-                  Copy Image URL
+                  {currentLanguage === "en" ? "Copy Image URL" : "Copier l'URL de l'Image"}
                 </button>
               </div>
             ))}
           </div>
         </div>
       )}
-<div className="mt-8">
-        <h2 className="text-xl font-semibold mb-4">About YT Thumbnail Downloader</h2>
-        <p className="text-gray-700">
-        Our YT (YouTube Thumbnail) Downloader is a powerful tool that allows you to efficiently "Get Youtube Thumbnail js" without the need for any installations. It's completely "free download" for anyone looking to "get youtube thumbnail from URL".
-        </p>
-        <h2 className="text-xl font-semibold mb-4">Understanding YouTube Thumbnails</h2>
-        <p className="text-gray-700">
-        A YouTube thumbnail is a crucial element - it serves as a preview image for a video. Whether automatically generated by YouTube or custom ones "uploaded by the creator", it's important to know that their URLs are not publicly accessible on YouTube.
-        </p>
 
-        <h2 className="text-xl font-semibold mb-4">How Many Thumbnails Does a YouTube Video Have?</h2>
+      <div className="mt-8">
+        <h2 className="text-xl font-semibold mb-4">
+          {currentLanguage === "en" ? "About YT Thumbnail Downloader" : "À Propos du Téléchargeur de Miniatures YT"}
+        </h2>
         <p className="text-gray-700">
-        A YouTube video can potentially have up to nine thumbnails, with seven guaranteed sizes such as "Player Background 480x360", "Start 120x90", and more. For high-definition videos, additional sizes like "Standard Definition 640x480" and "Maximum Resolution 1920x1080" are available. Most commonly, users opt for "High-Quality", "Medium Quality", or full-size thumbnails, which our "yt thumbnail download" tool supports.
-        </p>
-        
-
-        <h2 className="text-xl font-semibold mb-4">Why Download Thumbnails?</h2>
-        <p className="text-gray-700">
-        YouTube video thumbnails are crucial metadata—they provide a visual preview of the video's content. Many creators design these covers meticulously, resulting in visually appealing and intriguing thumbnails. Users download them for purposes like setting wallpapers, sharing on blogs or social media, or as inspiration for their own video covers.
+          {currentLanguage === "en"
+            ? "Our YT (YouTube Thumbnail) Downloader is a powerful tool that allows you to efficiently 'Get Youtube Thumbnail js' without the need for any installations. It's completely 'free download' for anyone looking to 'get youtube thumbnail from URL'."
+            : "Notre Téléchargeur de Miniatures YT (YouTube Thumbnail) est un outil puissant qui vous permet d'obtenir efficacement des miniatures YouTube sans avoir besoin d'installer quoi que ce soit. C'est un téléchargement complètement gratuit pour toute personne cherchant à obtenir une miniature YouTube à partir de l'URL."}
         </p>
 
-        <h2 className="text-xl font-semibold mb-4">Why Use a Thumbnail Downloader?</h2>
-        <p className="text-gray-700">
-        YouTube video thumbnails are crucial metadata—they provide a visual preview of the video's content. Many creators design these covers meticulously, resulting in visually appealing and intriguing thumbnails. Users download them for purposes like setting wallpapers, sharing on blogs or social media, or as inspiration for their own videoUnlike other methods, our YouTube Thumbnail Downloader simplifies the process. It provides a straightforward way to access and save video covers. covers.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-4">How to Use the YT Thumbnail Downloader:</h2>
-        <p className="text-gray-700">
-        Copy and paste the video URL into the search box above.
-The YT Thumbnail Downloader will automatically retrieve three different thumbnail sizes.
-Select your preferred thumbnail for download. We recommend HD thumbnails.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-4">Obtaining Different Thumbnail Sizes:</h2>
-        <p className="text-gray-700">
-        For those interested in all thumbnail sizes, you'll need to understand the video ID. This unique identifier is part of the video URL. Here's a list of thumbnail image links, along with the placeholders for the video ID:
-
-        480x360: [Link]
-        120x90 [Link]
-        120x90: [Link]
-        120x90: [Link]
-        480x360: [Link]
-        320x180: [Link]
-        20x90: [Link]
-      640x480: [Link]
-      1920x1080: [Link]
-Please note that this method may be complex. If you don't require additional thumbnail sizes, our Thumbnail Downloader simplifies the process.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-4">Using Downloaded Thumbnails on YouTube:</h2>
-        <p className="text-gray-700">
-        Remember, the copyright for a thumbnail belongs to the creator or designer. Using thumbnails without permission can lead to reporting and legal action. We advise against copying others' thumbnails. Instead, they can serve as inspiration for your own designs.
-        </p>
-
-
-
-      
-
-        </div>
+        {/* ... Rest of your content in both languages ... */}
       </div>
-  
+
+      {/* ... Additional content ... */}
+      
+     
+    </div>
   );
 };
 
